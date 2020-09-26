dd2a60754512259d4783978aca4c067406892063
Cesar Martinez <cesaguma@hotmail.com>
2020-09-25 23:32:27 -0400

    Testing connection between index.html and script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 //var iconcode = a.weather[0].icon;
 var iconcode = "10n";
-var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png"; // Getting icon from the weather web page
 console.log(iconurl);
 $('#wiconHeader').attr('src', iconurl);
